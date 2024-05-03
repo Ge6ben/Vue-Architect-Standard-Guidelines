2bb8b42ca2e723f272d3ca3cfe94fd7ae11e734f
Ge6ben <dl22gesh40@gmail.com>
2024-05-03 05:24:19 +0300

    ci(esLint):config prettier with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,12 +9,30 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-  ],
+
+  extends: ['plugin:vue/vue3-recommended', 'eslint:recommended', 'prettier'],
+  plugins: ['vue', 'prettier'],
+
   rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        arrowParens: 'avoid',
+        bracketSpacing: true,
+        endOfLine: 'lf',
+        bracketSameLine: false,
+        jsxSingleQuote: false,
+        printWidth: 80,
+        proseWrap: 'preserve',
+        quoteProps: 'as-needed',
+        semi: true,
+        singleQuote: true,
+        tabWidth: 2,
+        trailingComma: 'all',
+        useTabs: false,
+      },
+    ],
     'vue/multi-word-component-names': 'off',
   },
-}
+  ignorePatterns: ['dist'],
+};
