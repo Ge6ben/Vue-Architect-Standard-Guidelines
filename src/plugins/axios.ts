5df137d9b9e9d770c1113c59e91e576bb1935c31
Ge6ben <dl22gesh40@gmail.com>
2024-05-08 04:41:30 +0300

    ci(axios):add interceptor

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import router from '@/router';
+import { alertService } from '@/services/alertServices';
 
 // Create an Axios instance with a base URL and other configurations
 const axiosInstance = axios.create({
@@ -8,5 +10,46 @@ const axiosInstance = axios.create({
   },
   timeout: 10000, // Timeout after 10 seconds
 });
+
+// Adding request interceptor
+axiosInstance.interceptors.request.use(
+  config => {
+    // Perform actions before request is sent
+    console.log('Sending request to', config.url);
+    return config;
+  },
+  error => {
+    // Do something with request error
+    return Promise.reject(error);
+  },
+);
+
+// Adding response interceptor
+axiosInstance.interceptors.response.use(
+  response => {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    console.log('Getting Response', response);
+
+    return response;
+  },
+  error => {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    if (error.response.status === 401) {
+      router.push('/login');
+      alertService.danger('Session Expired, please login again.');
+    } else if (error.response.status === 422) {
+      for (const [key, value] of Object.entries(
+        error.response?.data.errors || {},
+      )) {
+        alertService.danger(error.response?.data.errors[key][0]);
+      }
+    } else if (error.response.status === 500 || error.response.status === 404) {
+      alertService.danger(error.response?.data.message);
+    }
+    return Promise.reject(error);
+  },
+);
 
 export default axiosInstance;
